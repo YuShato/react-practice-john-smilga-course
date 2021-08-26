2b9265bd3cbfbb2d20118354bf742816ef9c0c4f
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-08-26 20:40:26 +0300

    Добавляет проект №15. Коктейльная карта

---
+++ 15-coctails/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium perferendis aspernatur repudiandae. Accusantium vitae sunt libero aut similique, quas esse porro. Consectetur, sunt? Veritatis, quisquam soluta harum labore quia numquam qui suscipit, repudiandae, rem ducimus dolores nihil! A quae odit nihil aliquid mollitia asperiores, ratione sequi praesentium. Deleniti, eos. Consequatur fugit adipisci nobis id illum obcaecati, deserunt necessitatibus minus possimus praesentium, libero a voluptas deleniti, quae nisi suscipit aperiam tenetur! Repellendus sint temporibus culpa hic provident fuga quidem saepe ea reiciendis, ad, deserunt accusantium? Fugiat distinctio, perferendis dolorum quasi numquam, cumque, ab illo quis autem facere modi quae nam veniam!</p>
+    </section>
+  )
+}
+
+export default About
